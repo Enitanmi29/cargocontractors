dac092ef66d3bf9a1e97b5a1f3b2bbe775a6fbca
tshaba29 <abidakuntaiwoenitan@gmail.com>
2025-04-07 20:10:16 +0100

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 
 const About = () => {
@@ -30,37 +29,4 @@ const About = () => {
   )
 }
 
-=======
-import React from 'react'
-
-const About = () => {
-  return (
-    <div>
-    <div className='row text-center pt-5 bg-secondary-subtle'>
-        <div className='col'>
-            <h1 className='pb-1'> About Us:</h1>
-        </div>
-        <div className='row justify-content-center'>
-            <p className='fs-3'> 
-                We are the premier import and export agency in Africa. Our network spreads to differet part of the word.
-            </p>
-            <div className='row rounded-2 my-4'>
-                <img
-                src='/ship2.jpg'
-                alt='cargo    '
-                className='col-lg-6 img-fluid rounded-circle mb-2 pb-2'
-                />
-                <img
-                src='/IMG-20250316-WA0034.jpg'
-                alt='cargo    '
-                className='col-lg-6 img-fluid rounded-circle mb-2 pb-2'
-                />
-            </div>
-        </div>
-    </div>
-    </div>
-  )
-}
-
->>>>>>> bfdba15c396be617e0b40f5816eac1a9d786b7fa
 export default About
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 
 const Footer = () => {
@@ -18,25 +17,4 @@ const Footer = () => {
   )
 }
 
-=======
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div className="container">
-        <footer className="py-3">
-            <hr/>
-            <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-            <li className="nav-item"><a href="/" class="nav-link px-2 text-body-secondary">Home</a></li>
-            <li className="nav-item"><a href="/about" class="nav-link px-2 text-body-secondary">About</a></li>
-            <li className="nav-item"><a href="/services" class="nav-link px-2 text-body-secondary">Services</a></li>
-            <li className="nav-item"><a href="/contact" class="nav-link px-2 text-body-secondary">Contacts</a></li>
-            </ul>
-            <p className="text-center text-body-secondary">© {new Date().getFullYear()} Cargo Contractors.</p>
-        </footer>
-    </div>
-  )
-}
-
->>>>>>> bfdba15c396be617e0b40f5816eac1a9d786b7fa
 export default Footer
