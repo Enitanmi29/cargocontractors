ef99ddbcc4132546af2ffcae7a732ba6ea800b8d
tshaba29 <abidakuntaiwoenitan@gmail.com>
2025-04-07 19:29:27 +0100

    commit whatsapp

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Services from './components/Services'
+import Contact from './components/Contact'
+import About from './components/About'
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <Router>
+            <Navbar />
+        <div className='bg-tertiary'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/services' element={<Services />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </div>
+        <Footer />
+    </Router>
+  )
+}
+
+export default App
---
+++ src/constants/index.jsx
@@ -0,0 +1,63 @@
+export const contact = [
+{ header:'Mobile',
+  paragraph:'08059780090',
+  link:'https://whatsapp.com/dl/'
+},
+{ header:'GMail',
+    paragraph:'Cargocontractors@gmail.com',
+    link:'mailto:abidakuntaiwoenitan@gmail.com?subject=Inquiry&body=hello%20cargo%20contactors'
+},
+{ header:'Instagram',
+    paragraph:'@Cargo_contractors.</',
+    link:'https://www.instagram.com/yung_josht?igsh=MTBxYzFzMDR5bmwwZQ=='
+},
+{ header:'Twitter',
+    paragraph:'@Cargo_contractors.</',
+    link:'https://x.com/abidakun_t28291?t=RR1J4RDsW4abwPP9faCGHw&s=08'
+},
+]
+
+export const contact1 = [
+    { header:'Mobile',
+      paragraph:'08059780090',
+        
+      },
+      { header:'GMail',
+        paragraph:'Cargocontractors@gmail.com',
+          
+      },
+      { header:'Instagram',
+        paragraph:'@Cargo_contractors.</',
+          
+      },
+      { header:'Twitter',
+        paragraph:'@Cargo_contractors.</',
+          
+      },
+]
+
+export const services = [
+    { header:'Clearing',
+      paragraph:'Paragraph of text beneath the heading to explain the heading.',
+
+    },
+    { header:'Forwarding',
+        paragraph:'Paragraph of text beneath the heading to explain the heading.',
+        
+    },
+    { header:'Demourage',
+        paragraph:'Paragraph of text beneath the heading to explain the heading.',   
+    },
+    { header:'Car sale',
+        paragraph:'Paragraph of text beneath the heading to explain the heading.',
+    },
+]
+
+export const services1 = [
+    { header:'Clearing',
+      paragraph:'Paragraph of text beneath the heading to explain the heading.',
+    },
+    { header:'Forwarding',
+        paragraph:'Paragraph of text beneath the heading to explain the heading.',    
+    },  
+]
